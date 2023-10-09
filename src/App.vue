74cfd37c9d25d6aa03ec9e28fa5eca4624e64e1a
Rafa <arruda.rafa.ra@gmail.com>
2023-10-09 08:37:45 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+
+    <div class="column is-three-quarter">
+      <Formulario @aoSalvarTarefa="salvarTarefa" />
+      <div class="lista">
+        
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <Box v-if="listaVazioTarefa">
+            Você não está muito produtivo hoje :(
+        </Box>
+      </div>
+    </div>
+
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
+import Tarefa from './components/Tarefa.vue';
+import Box from './components/Box.vue';
+import ITarefa from './interfaces/ITarefa'
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    Formulario,
+    Tarefa,
+    Box
+  },
+
+  data() {
+    return {
+      tarefas: [] as ITarefa[]
+    }
+  },
+
+  computed : {
+    listaVazioTarefa () : boolean{
+      return this.tarefas.length == 0
+
+    }
+
+
+  },
+
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    }
+  }
+
+});
+</script>
+
+<style>
+.lista {
+  padding: 1.25rem;
+}
+</style>
---
+++ src/components/Cronometro.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <strong>
+            {{ tempoDecorrido }}
+        </strong>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'Cronometro',
+    props: {
+        tempoEmSegundos: {
+            type: Number,
+            default: 0
+        }
+    },
+
+    computed:{ //função espera receber uma função getter e o valor retornado é um ref calculado
+        tempoDecorrido () : string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }
+
+    },
+
+
+})
+</script>
+
+<style scoped></style>
